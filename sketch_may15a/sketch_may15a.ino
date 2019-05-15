bad3f2e9c0f472e1fd07a57d314dc5781110efff
azakenaton <luccho7.lh@gmail.com>
2019-05-15 19:46:41 +0200

    Midofication

--- sketch_may15a/sketch_may15a.ino
+++ sketch_may15a/sketch_may15a.ino
@@ -2,34 +2,64 @@
 
 int posX;
 int posY;
-int speedX = 1;
+double vit = 2;
+Player *p;
+
+class Player 
+{
+  private:
+  
+  int x;
+  int y;
+
+  public:
+
+  Player::Player()
+  {
+    x = (gb.display.width()/2)-2;
+    y = (gb.display.height()/2)-2;
+    this.setColor(WHITE);
+    gb.display.fillRect(x, y, 4, 4);
+  }
+  
+};
 
 void setup() {
   gb.begin();
-  posX = (gb.display.width()/2)-2;
-  posY = (gb.display.height()/2)-2;
+  p = new Player; 
 }
 
+void moove_player(){
+  
+  if(gb.buttons.repeat(BUTTON_UP,1)){
+    if (posY > 0){
+      posY -= vit;
+    }
+   }
+   if(gb.buttons.repeat(BUTTON_DOWN,1)){
+    if (posY < gb.display.height()-4){
+      posY += vit;
+    }
+   }
+   if(gb.buttons.repeat(BUTTON_LEFT,1)){
+     if (posX > 0){
+      posX -= vit;
+    }
+   }
+   if(gb.buttons.repeat(BUTTON_RIGHT,1)){
+     if (posX < gb.display.width()-4){
+      posX += vit;
+    }
+   }
+
+   gb.display.setColor(WHITE);
+   gb.display.fillRect(posX, posY, 4, 4);
+}
 
 void loop() {
   while(!gb.update());
   gb.display.clear();
 
-  if(gb.buttons.repeat(BUTTON_UP,1)){
-    posY -= 1;
-  }
-  if(gb.buttons.repeat(BUTTON_DOWN,1)){
-    posY += 1;
-  }
-  if(gb.buttons.repeat(BUTTON_LEFT,1)){
-    posX -= 1;
-  }
-  if(gb.buttons.repeat(BUTTON_RIGHT,1)){
-    posX += 1;
-  }
-
-  
-  gb.display.setColor(WHITE);
-  gb.display.fillRect(posX, posY, 4, 4);
+  moove();
   
 }
