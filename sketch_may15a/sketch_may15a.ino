7acd82efa5b6117a9b99277ba0ccc651eca4b5d4
azakenaton <luccho7.lh@gmail.com>
2019-05-15 14:07:15 +0200

    Ajout du projet

---
+++ sketch_may15a/sketch_may15a.ino
@@ -0,0 +1,27 @@
+#include <Gamebuino-Meta.h>
+
+int posX = 1;
+int speedX = 1;
+
+void setup() {
+  gb.begin();
+}
+
+
+void loop() {
+  while(!gb.update());
+  gb.display.clear();
+
+  if(posX = gb.display.width()){
+    speedX = -1;
+  }
+  if(posX = 0){
+    speedX = 1;
+  }
+
+  posX = posX + speedX;
+  
+  gb.display.setColor(BROWN);
+  gb.display.fillRect(posX, 32, 4, 4);
+  
+}
