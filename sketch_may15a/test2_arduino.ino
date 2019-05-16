82ca836d2a8ea9717b5043867d7070f5eb16c9e0
laurent <laurent.mazoyer@orange.fr>
2019-05-16 11:34:08 +0200

    add items top

--- sketch_may15a/test2_arduino.ino
+++ sketch_may15a/test2_arduino.ino
@@ -1,12 +1,12 @@
 #include <Gamebuino-Meta.h>
 
 // Items
-const int nbrItems = 7;
+const int nbrItems = 6;
 int itemGaucheX[nbrItems] = {};
 int itemGaucheY[nbrItems] = {};
 
-int itemDroiteX[nbrItems] = {};
-int itemDroiteY[nbrItems] = {};
+int itemHautX[nbrItems] = {};
+int itemHautY[nbrItems] = {};
 
 const int itemLargeur = 2;
 const int itemHauteur = 2;
@@ -15,10 +15,10 @@ void setup() {
    gb.begin();
    for (int i = 0; i < nbrItems; i += 1) {
     itemGaucheX[i] = random(-40, 0);
-    itemGaucheY[i] = random(0, gb.display.height());  // Au dessus de l'écran
+    itemGaucheY[i] = random(0, gb.display.height()); 
 
-    itemDroiteX[i] = random( gb.display.width(),gb.display.width()+40);
-    itemDroiteY[i] = random(0, gb.display.height());
+    itemHautX[i] = random(0, gb.display.width());  
+    itemHautY[i] = random(-40, 0); // Au dessus de l'écran
     
   }
 }
@@ -28,7 +28,7 @@ void loop() {
   gb.display.clear();
   for (int i = 0; i < nbrItems; i += 1) {     
     itemGaucheX[i] += 1;
-    itemDroiteX[i] -= 1;
+    itemHautY[i] += 1;
 
 
   if (itemGaucheX[i]>= gb.display.width()){
@@ -36,14 +36,14 @@ void loop() {
     itemGaucheY[i] = random (0, gb.display.height());
   }
 
-   if (itemDroiteX[i] <= 0){
-    itemDroiteX[i] =  random( gb.display.width(),gb.display.width()+40);
-    itemDroiteY[i] = random (0, gb.display.height());
+   if (itemHautY[i] >= gb.display.height()){
+    itemHautX[i] =  random(0, gb.display.width());
+    itemHautY[i] = random(-40, 0); // Au dessus de l'écran
   }
   
   for (int j = 0; j < nbrItems; j += 1) {
     gb.display.fillRect(itemGaucheX[j], itemGaucheY[j], itemLargeur, itemHauteur);
-    gb.display.fillRect(itemDroiteX[j], itemDroiteY[j], itemLargeur, itemHauteur);
+    gb.display.fillRect(itemHautX[j], itemHautY[j], itemLargeur, itemHauteur);
   }    
   }
 }
